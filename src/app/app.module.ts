62c1fb4788e4daef506d18064c79e85b741e9df8
VICJR31 <73763182+VICJR31@users.noreply.github.com>
2021-10-07 10:58:49 -0700

    edits for firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,9 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-// (10/5/21) Stopped Headers. Finish on Wednesday AND start like/dislike system!
+// (10/5/21) Stopped Headers. Finish on Wednesday(Thursday) AND start like/dislike system!
 import { AngularFireModule } from '@angular/fire';
-import { enviroment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment';
 // import { WorldWarOneComponent } from './world-war-one/world-war-one.component';
 // import { WorldWarTwoComponent} from './world-war-two/world-war-two.component';
 
@@ -20,7 +20,13 @@ import { enviroment } from 'src/environments/environment';
     // WorldWarTwoComponent
   ],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    // (10/7/21) get the fireservice.ts files in here for imports!
+    AngularFireModule.initializeApp(environment.firebase),
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
