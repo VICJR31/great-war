6f0928bf32cfaed9f2c79b4dac35e9d09361d067
VICJR31 <73763182+VICJR31@users.noreply.github.com>
2021-10-18 14:31:16 -0700

    remove firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,8 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-// (10/5/21) Stopped Headers. Finish on Wednesday(Thursday) AND start like/dislike system!
-import { AngularFireModule } from '@angular/fire';
 import { environment } from 'src/environments/environment';
 // import { WorldWarOneComponent } from './world-war-one/world-war-one.component';
-// import { WorldWarTwoComponent} from './world-war-two/world-war-two.component';
 
 @NgModule({
   declarations: [
@@ -21,13 +18,13 @@ import { environment } from 'src/environments/environment';
   ],
   entryComponents: [],
   imports: [
-    // (10/7/21) get the fireservice.ts files in here for imports!
-    AngularFireModule.initializeApp(environment.firebase),
     BrowserModule, 
     IonicModule.forRoot(), 
     AppRoutingModule
   ],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
