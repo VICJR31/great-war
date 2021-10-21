a9d0b41c7aa81afdfbdd2433d7b790c3514572ff
VICJR31 <73763182+VICJR31@users.noreply.github.com>
2021-10-21 15:50:23 -0700

    login UI added with basic backend

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -7,14 +7,16 @@ import { IonicModule } from '@ionic/angular';
 import { LoginPageRoutingModule } from './login-routing.module';
 
 import { LoginPage } from './login.page';
+import { InputModule } from '../input/input.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    InputModule,
     LoginPageRoutingModule
   ],
-  declarations: [LoginPage]
+  declarations: [LoginPage],
 })
 export class LoginPageModule {}
