2c859ee053e700b15671e4cc695448761d8500f4
VICJR31 <73763182+VICJR31@users.noreply.github.com>
2021-10-22 12:48:18 -0700

    Basic UI for signup page done

--- src/app/signup/signup.module.ts
+++ src/app/signup/signup.module.ts
@@ -7,12 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { SignupPageRoutingModule } from './signup-routing.module';
 
 import { SignupPage } from './signup.page';
+import { InputModule } from '../input/input.module';
+
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    InputModule,
     SignupPageRoutingModule
   ],
   declarations: [SignupPage]
