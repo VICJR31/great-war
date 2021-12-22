b0716bb41f50276d9dcf5a5a25c97c7919935b54
VICJR31 <73763182+VICJR31@users.noreply.github.com>
2021-12-22 14:52:12 -0800

    name change

--- server.js
+++ server.js
@@ -28,10 +28,10 @@ const path = require('path');
 const app = express();
 
 //  Serve only the static files form the dist directory
-app.use(express.static('./dist/myAppName'));
+app.use(express.static('./dist/great-war'));
 
 app.get('/*', (req, res) => 
-    res.sendFile('index.html', {root: 'dist/myAppName'}),
+    res.sendFile('index.html', {root: 'dist/great-war'}),
 );
 
 //  Start the app by listening on the default Heroku port
