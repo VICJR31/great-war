271c4198481a86980752870c63faf8fa393b440c
VICJR31 <73763182+VICJR31@users.noreply.github.com>
2021-12-22 14:26:04 -0800

    change deps

--- server.js
+++ server.js
@@ -1,23 +1,38 @@
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
+// function requireHTTPS(req, res, next) {
+//     // The 'x-forwarded-proto' check is for Heroku
+//     if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+//         return res.redirect('https://' + req.get('host') + req.url);
+//     }
+//     next();
+// }
+
+// const express = require('express');
+// const app = express();
+
+// app.use(requireHTTPS);
+
+// app.use(express.static('./dist/great-war'));
+
+// app.get('/*', function(req, res) {
+//     res.sendFile('www/index.html', {root: 'dist/great-war/'}
+//   );
+// });
+
+// app.listen(process.env.PORT || 21111);
+
+// console.log("Listening in on PORT 21111...");
 
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.use(requireHTTPS);
-
+//  Serve only the static files form the dist directory
 app.use(express.static('./dist/great-war'));
 
-app.get('/*', function(req, res) {
-    res.sendFile('www/index.html', {root: 'dist/great-war/'}
-  );
-});
+app.get('/*', (req, res) => 
+    res.sendFile('index.html', {root: 'dist/great-war'}),
+);
 
-app.listen(process.env.PORT || 21111);
-
-console.log("Listening in on PORT 21111...");
+//  Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
