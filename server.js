240187ccff56b1507805c20024833fb52118322f
VICJR31 <73763182+VICJR31@users.noreply.github.com>
2021-12-23 15:32:20 -0800

    build add with --prod

--- server.js
+++ server.js
@@ -36,3 +36,6 @@ app.get('/*', (req, res) =>
 
 //  Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+// removed from package.json underneath e2e
+// "heroku-postbuild": "ng build --prod"
