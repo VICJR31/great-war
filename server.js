4a60f5f7df9f0b6526f908eecf11a3a70bf1a293
VICJR31 <73763182+VICJR31@users.noreply.github.com>
2021-12-23 16:09:32 -0800

    remove notes

--- server.js
+++ server.js
@@ -1,27 +1,3 @@
-// function requireHTTPS(req, res, next) {
-//     // The 'x-forwarded-proto' check is for Heroku
-//     if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-//         return res.redirect('https://' + req.get('host') + req.url);
-//     }
-//     next();
-// }
-
-// const express = require('express');
-// const app = express();
-
-// app.use(requireHTTPS);
-
-// app.use(express.static('./dist/great-war'));
-
-// app.get('/*', function(req, res) {
-//     res.sendFile('www/index.html', {root: 'dist/great-war/'}
-//   );
-// });
-
-// app.listen(process.env.PORT || 21111);
-
-// console.log("Listening in on PORT 21111...");
-
 const express = require('express');
 const path = require('path');
 
