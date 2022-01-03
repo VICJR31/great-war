e6686d7b3ff66b117abcfcbccf9279858e476faf
VICJR31 <73763182+VICJR31@users.noreply.github.com>
2022-01-02 17:01:08 -0800

    discord bot code

--- server.js
+++ server.js
@@ -3,6 +3,22 @@ const path = require('path');
 const mongoose = require('mongoose');
 const app = express();
 
+// For Discord bot (start: 'node .')
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const token = "bot token here";
+
+client.on('ready', () => {
+    console.log('Ready!');
+});
+
+client.on('message', message => {
+   if (message.content === '!ping') {
+       message.channel.send('Pong.');
+}
+});
+
+
 // var mongoUrl = '"mongodb+srv://dbAlex:idk1234@greatwarcluster0.g3qjw.mongodb.net/Data"'
 // var mongoose = require('mongoose');
 
@@ -33,7 +49,8 @@ app.get('*', function (req, res) {
 
 //  Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-console.log("Listening in on PORT 8080")
+console.log("Listening in on PORT 8080");
+client.login(token);
 
 // removed from package.json underneath e2e
 // "heroku-postbuild": "ng build --prod"
